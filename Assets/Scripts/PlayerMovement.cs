284ceac7f55ba17cedacb711377f4be0321e9a33
Ong Wei Yang <ong.weiyang23@gmail.com>
2026-09-21 00:09:36 +0800

    completed until mario flips

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,9 +7,11 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 10;
     public float maxSpeed = 20;
-    private Rigidbody2D marioBody;
-
     public float upSpeed = 10;
+    private Rigidbody2D marioBody;
+    private SpriteRenderer marioSprite;
+    private bool faceRightState = true;
+    
     private bool onGroundState = true;
 
     void OnCollisionEnter2D(Collision2D col)
@@ -20,9 +22,11 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
         // Set to be 30 FPS
         Application.targetFrameRate = 30;
         marioBody = GetComponent<Rigidbody2D>();
+        marioSprite = GetComponent<SpriteRenderer>();
 
     }
 
@@ -45,10 +49,21 @@ public class PlayerMovement : MonoBehaviour
                 marioBody.AddForce(movement * speed);
         }
 
+        // toggle state
+        if (Input.GetKeyDown("a") && faceRightState){
+            faceRightState = false;
+            marioSprite.flipX = true;
+        }
+
+        if (Input.GetKeyDown("d") && !faceRightState)
+        {
+            faceRightState = true;
+            marioSprite.flipX = false;
+        }
+
         // stop
         if (Input.GetKeyUp("a") || Input.GetKeyUp("d"))
         {
-            // stop
             marioBody.linearVelocity = Vector2.zero;
         }
 
@@ -58,7 +73,5 @@ public class PlayerMovement : MonoBehaviour
             onGroundState = false;
         }
 
-        //Vector2 movement = new Vector2(moveHorizontal, 0);
-        //marioBody.AddForce(movement * speed);
     }
 }
