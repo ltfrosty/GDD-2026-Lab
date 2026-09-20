f5e0ce65ca0857bff55f9fdd4d44290a95cfc0dc
Ong Wei Yang <ong.weiyang23@gmail.com>
2026-09-20 22:06:43 +0800

    mario jumps!

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10;
+    public float maxSpeed = 20;
+    private Rigidbody2D marioBody;
+
+    public float upSpeed = 10;
+    private bool onGroundState = true;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Ground")) onGroundState = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set to be 30 FPS
+        Application.targetFrameRate = 30;
+        marioBody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // FixedUpdate is called 50 times a second
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Mathf.Abs(moveHorizontal) > 0)
+        {
+            Vector2 movement = new Vector2(moveHorizontal, 0);
+            // check if it doesn't go beyond maxSpeed
+            if (marioBody.linearVelocity.magnitude < maxSpeed)
+                marioBody.AddForce(movement * speed);
+        }
+
+        // stop
+        if (Input.GetKeyUp("a") || Input.GetKeyUp("d"))
+        {
+            // stop
+            marioBody.linearVelocity = Vector2.zero;
+        }
+
+        if (Input.GetKeyDown("space") && onGroundState)
+        {
+            marioBody.AddForce(Vector2.up * upSpeed, ForceMode2D.Impulse);
+            onGroundState = false;
+        }
+
+        //Vector2 movement = new Vector2(moveHorizontal, 0);
+        //marioBody.AddForce(movement * speed);
+    }
+}
