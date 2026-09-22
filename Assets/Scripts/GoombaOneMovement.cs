ffbab42598ee403a4ac738d4a23964af59c11df0
Shonim <shonimshaheen@gmail.com>
2026-09-22 15:26:33 +0800

    GOOMBA IS HERE, now we move to another branch cuz I wannna kekeep this code for fun :bulb:

---
+++ Assets/Scripts/GoombaOneMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoombaOneMovement : MonoBehaviour
+{
+    private float originalX;
+    private float maxOffset = 5.0f;
+    private float enemyPatroltime = 2.0f;
+    private int moveRight = -1;
+    private Vector2 velocity;
+
+    private Rigidbody2D enemyBody;
+
+    void Start()
+    {
+        enemyBody = GetComponent<Rigidbody2D>();
+
+        // Get the starting position
+        originalX = transform.position.x;
+
+        ComputeVelocity();
+    }
+
+    void ComputeVelocity()
+    {
+        velocity = new Vector2(
+            (moveRight) * maxOffset / enemyPatroltime,
+            0
+        );
+    }
+
+    void Movegoomba()
+    {
+        enemyBody.MovePosition(
+            enemyBody.position + velocity * Time.fixedDeltaTime
+        );
+    }
+
+    void Update()
+    {
+        if (Mathf.Abs(enemyBody.position.x - originalX) < maxOffset)
+        {
+            // Move Goomba
+            Movegoomba();
+        }
+        else
+        {
+            // Change direction
+            moveRight *= -1;
+            ComputeVelocity();
+            Movegoomba();
+        }
+    }
+}
